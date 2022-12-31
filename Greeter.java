45d0a41169de0126193e7c7abf92c205f2906a92
jenkinuser <christinaipsr1@gmail.com>
2022-12-31 08:29:37 +0530

    final commit1

--- Greeter.java
+++ Greeter.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Greeter {
     public static void main(String[] args)
     { 
-        System.out.println("What is your name? (Please provide your name and press enter)");
+        System.out.println("What is your full name? (Please provide your name and press enter)");
 
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
 
-        System.out.println("Hello " + name +  "!!");
+        System.out.println("Hello Jenkins " + name +  "!!");
 
     }
 }
+
